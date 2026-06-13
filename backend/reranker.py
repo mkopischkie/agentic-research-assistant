2e6da21c4b6647b5a4b39e69e84e8965666c3bdc
Melina Kopischkie <mkopischkie@gmail.com>
2026-06-13 14:44:28 -0600

    using cross encoder to rerank documents and return top 5

---
+++ backend/reranker.py
@@ -0,0 +1,19 @@
+# reranking info: https://medium.com/@rossashman/the-art-of-rag-part-3-reranking-with-cross-encoders-688a16b64669
+# good video: https://www.youtube.com/watch?v=I9lkQ_eudRA
+# rescore results from retrieval with cross-encoding
+# could do cross-encoding and LLM reranker (more semantic precision, reasoning)
+# the reranker (cross-encoding) uses a transformer architecture with scaled dot product attention to find how relevant a document/chunk is to the query
+# cross encoding has low latency and is very quick
+
+from sentence_transformers import CrossEncoder
+
+model = CrossEncoder("cross-encoder/ms-marco-MiniLM-L-6-v2") # from huggingface
+
+def reranker(query: str, chunks: list[str]):
+    sentence_pairs = [(query, chunk) for chunk in chunks]
+    scores = model.predict(sentence_pairs)
+    ranked = sorted(zip(chunks, scores), key=lambda x: x[1], reverse=True) # sticks chunks and scores together like [("a": 0.2), ("b", 0.6)] and sorts by largest score
+    return ranked[:5]
+
+# test = reranker('How many ducks are in the pond?', ['The pond is green', 'Jimmy sees 5 ducks in the pond', 'Anna sees a deer in the woods'])
+# print(test)
